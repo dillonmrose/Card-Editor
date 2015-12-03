eb6c719a9d17cc2d48f259a476f760d6535cf543
Dillon Rose <DR@dillonmrose.com>
2015-12-02 21:58:41 -0800

    Added TB adjustments for most sets

--- src/CardEditorDriver.java
+++ src/CardEditorDriver.java
@@ -1,18 +1,16 @@
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.FontFormatException;
 import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.GraphicsEnvironment;
+import java.awt.Point;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
@@ -24,15 +22,14 @@ import java.util.List;
 import javax.imageio.ImageIO;
 
 public class CardEditorDriver {
-	static HashMap<String, List<Integer>> expansion2draw = new HashMap<String, List<Integer>>();
-	public static void main(String[] args) throws IOException, FontFormatException {
+	static String currentDirectory;
+	static HashMap<String, String> expansion2ExpansionCode = new HashMap<String, String>();
+	static HashMap<String, HashMap<String, List<Point>>> expansionCode2TBPoints = new HashMap<String, HashMap<String, List<Point>>>();
 
+	public static void main(String[] args) throws Exception {
 		init();
-		String directory = "/Volumes/Users/drose/Dropbox/Booze Cube/";
-		String expansion_code_map_file = directory + "Expansion Code Map.txt";
-		HashMap<String, String> expansion_code_map = getExpansionCodeMap(expansion_code_map_file);
 		
-		String data_file = directory + "LegacyCube.2015-09-14.txt";
+		String data_file = currentDirectory + "/public/LegacyCube.2015-09-14.txt";
 
 		FileReader fr = new FileReader(data_file);
 		BufferedReader br = new BufferedReader(fr);
@@ -44,49 +41,82 @@ public class CardEditorDriver {
 			String[] lineCols = line.split("\t");
 			System.out.println(lineCols[0]);
 			
-			String baseImageFile = directory + "Cards/Base/"
+			String baseImageFile = currentDirectory + "/cards/original/"
 					+ lineCols[colIndexMap.get("image_file")];
-			String boozeImageFile = directory + "Cards/Booze/"
+			String boozeImageFile = currentDirectory + "/cards/booze/"
 					+ lineCols[colIndexMap.get("image_file")];
-			String expansion_code = expansion_code_map.get(lineCols[colIndexMap.get("expansion")]);
-			String image_url = "http://magiccards.info/scans/en/"+expansion_code+"/"+lineCols[colIndexMap.get("number")]+".jpg";
+			String expansion_code = expansion2ExpansionCode.get(lineCols[colIndexMap.get("expansion")]);
+			String type = lineCols[colIndexMap.get("type")];
+			if(type.contains("Creature")){
+				type = "Creature";
+			}
+			else{
+				type = "NonCreature";
+			}
+			
+			//String image_url = "http://magiccards.info/scans/en/"+expansion_code+"/"+lineCols[colIndexMap.get("number")]+".jpg";
+			//downloadBaseImage(image_url, baseImageFile);
+			
 			String boozeText = "";
 			if (lineCols.length > colIndexMap.get("booze_text")) {
 				boozeText = lineCols[colIndexMap.get("booze_text")];
 			}
-			downloadBaseImage(image_url, baseImageFile);
-			boozifyImage(baseImageFile, boozeImageFile, boozeText, expansion_code);
+			boozifyImage(baseImageFile, boozeImageFile, boozeText, expansion_code, type);
 		}
 
 		br.close();
 	}
 
-	private static void init() {
-		List<Integer> defaultValues = new ArrayList<Integer>();
-		defaultValues.add(20);
-		defaultValues.add(285);
-		defaultValues.add(265);
-		defaultValues.add(120);
-		expansion2draw.put("Default", defaultValues);
+	private static void init() throws Exception {
+		currentDirectory = System.getProperty("user.dir");
+		String expansion2ExpansionCodeFile = currentDirectory + "/public/Expansion Code Map.txt";
+		String expansionCode2TBPointsFile = currentDirectory + "/public/Textbox Data.txt";
+		expansion2ExpansionCode = getExpansion2expansionCodeMap(expansion2ExpansionCodeFile);
+		expansionCode2TBPoints = getExpansionCode2TBPointsMap(expansionCode2TBPointsFile);
 	}
 
-	private static HashMap<String, String> getExpansionCodeMap(
-			String expansion_code_map_file) throws IOException {
-		HashMap<String, String> expansion_code_map = new HashMap<String, String>();
-		FileReader fr = new FileReader(expansion_code_map_file);
+	private static HashMap<String, String> getExpansion2expansionCodeMap(
+			String expansion2ExpansionCodeFile) throws Exception {
+		HashMap<String, String> map = new HashMap<String, String>();
+		FileReader fr = new FileReader(expansion2ExpansionCodeFile);
 		BufferedReader br = new BufferedReader(fr);
 		br.readLine();
 		
 		for (String line = br.readLine(); line != null; line = br.readLine()) {
 			String[] lineSplit = line.split("\t");
-			expansion_code_map.put(lineSplit[0], lineSplit[1]);
+			map.put(lineSplit[0], lineSplit[1]);
 		}
 		
 		br.close();
-		return expansion_code_map;
+		return map;
 	}
 
-	private static void downloadBaseImage(String image_url, String baseImageFile) throws IOException{
+	private static HashMap<String, HashMap<String, List<Point>>> getExpansionCode2TBPointsMap(
+			String expansionCode2TBPointsFile) throws Exception {
+		HashMap<String, HashMap<String, List<Point>>> map = new HashMap<String, HashMap<String, List<Point>>>();
+		FileReader fr = new FileReader(expansionCode2TBPointsFile);
+		BufferedReader br = new BufferedReader(fr);
+		br.readLine();
+		
+		for (String line = br.readLine(); line != null; line = br.readLine()) {
+			String[] lineSplit = line.split("\t");
+			if(!map.containsKey(lineSplit[0])){
+				map.put(lineSplit[0], new HashMap<String, List<Point>>());
+			}
+			HashMap<String, List<Point>> type2TBPoints = map.get(lineSplit[0]);
+			List<Point> TBPoints = new ArrayList<Point>();
+			TBPoints.add(new Point(Integer.parseInt(lineSplit[2]), Integer.parseInt(lineSplit[3])));
+			TBPoints.add(new Point(Integer.parseInt(lineSplit[4]), Integer.parseInt(lineSplit[5])));
+			TBPoints.add(new Point(Integer.parseInt(lineSplit[6]), Integer.parseInt(lineSplit[7])));
+			type2TBPoints.put(lineSplit[1], TBPoints);
+			map.put(lineSplit[0], type2TBPoints);
+		}
+		
+		br.close();
+		return map;
+	}
+	
+	private static void downloadBaseImage(String image_url, String baseImageFile) throws Exception{
 		URL url = new URL(image_url);
         URLConnection connection = url.openConnection();
         InputStream input = connection.getInputStream();
@@ -100,17 +130,18 @@ public class CardEditorDriver {
 	}
 	
 	private static void boozifyImage(String baseImageFile,
-			String boozeImageFile, String boozeText, String expansion_code) throws IOException, FontFormatException {
+			String boozeImageFile, String boozeText, String expansion_code, String type) throws Exception {
 		BufferedImage bi = ImageIO.read(new File(baseImageFile));
 		if (boozeText.length() > 0) {
 
-			if(!expansion2draw.containsKey(expansion_code)){
+			if(!expansionCode2TBPoints.containsKey(expansion_code)){
 				expansion_code = "Default";
 			}
+			List<Point> TBPoints = expansionCode2TBPoints.get(expansion_code).get(type);
 			Graphics2D ig2 = bi.createGraphics();
 
-			clearTextBox(ig2, bi, expansion_code);
-			writeTextBox(ig2, boozeText, expansion_code);
+			clearTextBox(ig2, bi, TBPoints);
+			writeTextBox(ig2, boozeText, TBPoints);
 		}
 		ImageIO.write(bi, "PNG", new File(boozeImageFile));
 	}
@@ -137,12 +168,12 @@ public class CardEditorDriver {
 	}
 
 	private static void writeTextBox(Graphics2D ig2,
-			String text, String expansion_code) throws FileNotFoundException, FontFormatException, IOException {
-
-		int startWidth = expansion2draw.get(expansion_code).get(0);
-		int startHeight = expansion2draw.get(expansion_code).get(1);
-		int width = expansion2draw.get(expansion_code).get(2);
-		int height = expansion2draw.get(expansion_code).get(3);
+			String text, List<Point> TBPoints) throws Exception {
+		
+		int TLX = (int) TBPoints.get(0).getX();
+		int TLY = (int) TBPoints.get(0).getY();
+		int BRX1 = (int) TBPoints.get(1).getX();
+		int BRY1 = (int) TBPoints.get(1).getY();
 		
 		float fontSize = 15;
 		LinkedList<String> textLines = new LinkedList<String>();
@@ -168,7 +199,7 @@ public class CardEditorDriver {
 					String textToDraw = textTemp;
 					int stringWidth = fontMetrics.stringWidth(textToDraw);
 					stringHeight = (int) (fontMetrics.getAscent() * 1.1);
-					while (stringWidth > width) {
+					while (stringWidth > BRX1 - TLX) {
 						int endPoint = textToDraw.length();
 						if(textToDraw.contains(" ") && textToDraw.contains("\\n")){
 							endPoint = Math.max(textToDraw.lastIndexOf(' '), textToDraw.lastIndexOf('\n'));
@@ -193,7 +224,7 @@ public class CardEditorDriver {
 					counter++;
 				}
 			}
-			if (counter * stringHeight < height) {
+			if (counter * stringHeight < BRY1 - TLY) {
 				fits = true;
 			}
 			fontSize-=.3;
@@ -202,21 +233,24 @@ public class CardEditorDriver {
 
 
 		int counter = 1;
-		stringHeight = (int) (fontMetrics.getAscent() * 1.1);
+		stringHeight = (int) (fontMetrics.getAscent() * 1.2);
 		ig2.setPaint(Color.black);
 		for (String textLine : textLines) {
-			ig2.drawString(textLine, startWidth, startHeight + stringHeight * counter++);
+			ig2.drawString(textLine, TLX, TLY + stringHeight * counter++);
 		}
 	}
 
 	private static void clearTextBox(Graphics2D ig2,
-			BufferedImage bi, String expansion_code) {
-		int startWidth = expansion2draw.get(expansion_code).get(0);
-		int startHeight = expansion2draw.get(expansion_code).get(1);
-		int width = expansion2draw.get(expansion_code).get(2);
-		int height = expansion2draw.get(expansion_code).get(3);
-		int colorRGB = bi.getRGB(startWidth, startHeight);
+			BufferedImage bi, List<Point> TBPoints) {
+		int TLX = (int) TBPoints.get(0).getX();
+		int TLY = (int) TBPoints.get(0).getY();
+		int BRX1 = (int) TBPoints.get(1).getX();
+		int BRY1 = (int) TBPoints.get(1).getY();
+		int BRX2 = (int) TBPoints.get(2).getX();
+		int BRY2 = (int) TBPoints.get(2).getY();
+		int colorRGB = bi.getRGB(TLX, TLY);
 		ig2.setPaint(new Color(colorRGB));
-		ig2.fillRect(startWidth, startHeight, width, height);
+		ig2.fillRect(TLX, TLY, BRX1 - TLX, BRY1 - TLY);
+		ig2.fillRect(TLX, TLY, BRX2 - TLX, BRY2 - TLY);
 	}
 }
